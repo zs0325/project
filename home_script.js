90dd41042621808c434de9461ee3f1aa7b9e0259
zs0325 <143070945+zs0325@users.noreply.github.com>
2023-08-24 09:40:12 +0200

    Add files via upload

---
+++ home_script.js
@@ -0,0 +1,6 @@
+function redirect(){
+  window.location.href="stores.html";
+}
+function redirect2(){
+  window.location.href="menu.html";
+}
---
+++ stores_script.js
@@ -0,0 +1,19 @@
+function redirect() {
+  window.location.href = "home.html";
+}
+function options() {
+  document.getElementById("WC").classList.toggle("show");
+
+}
+function options2() {
+  document.getElementById("EC").classList.toggle("show");
+}
+function options3() {
+  document.getElementById("NC").classList.toggle("show");
+}
+function options4() {
+  document.getElementById("M").classList.toggle("show");
+}
+function options5() {
+  document.getElementById("KZN").classList.toggle("show");
+}
